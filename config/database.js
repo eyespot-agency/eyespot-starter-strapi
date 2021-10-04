688df40b287297c4c023cfb13fee3cb2f37948e7
Mehdi Mahmoudi <mehdi@emiketic.com>
2021-10-04 14:57:41 +0100

    feat: Upgraded dependencies + changed env. config + changed script names

--- config/database.js
+++ config/database.js
@@ -4,12 +4,14 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL'),
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
